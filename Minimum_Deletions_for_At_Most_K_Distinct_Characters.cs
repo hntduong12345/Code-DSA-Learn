d4851ea89817c7a576df93668e30e0088759ebd8
hntduong12345 <115139619+hntduong12345@users.noreply.github.com>
2025-12-31 11:16:42 +0700

    refactor: change code structure format
    From project to class to store code

---
+++ Minimum_Deletions_for_At_Most_K_Distinct_Characters.cs
@@ -0,0 +1,33 @@
+﻿class Solution
+{
+    private static void Main(string[] args)
+    {
+        string s = "abc";
+        int k = 2;
+        Console.WriteLine(MinDeletion(s, k));
+    }
+
+    public static int MinDeletion(string s, int k)
+    {
+        int[] count = new int[26];
+        int distintChar = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (count[s[i] - 'a']++ == 0)
+                distintChar++;
+        }
+
+        Array.Sort(count);
+        int result = 0;
+        for (int i = 0; i < count.Length; i++)
+        {
+            if (count[i] != 0 && distintChar - k > 0)
+            {
+                result += count[i];
+                k++;
+            }
+        }
+
+        return result;
+    }
+}
