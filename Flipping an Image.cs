74eb6d6b9aa8fbf2433502ce6e100f8f5cf9beca
hntduong12345 <115139619+hntduong12345@users.noreply.github.com>
2026-01-30 21:38:14 +0700

    feat: add Flipping an Image code

---
+++ Flipping an Image.cs
@@ -0,0 +1,22 @@
+﻿public class Solution
+{
+    public int[][] FlipAndInvertImage(int[][] image)
+    {
+        int n = image.Length;
+        int temp = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j <= n / 2; j++)
+            {
+                if (j <= n - 1 - j)
+                {
+                    temp = image[i][j];
+                    image[i][j] = image[i][n - 1 - j] == 1 ? 0 : 1;
+                    image[i][n - 1 - j] = temp == 1 ? 0 : 1;
+                }
+            }
+        }
+
+        return image;
+    }
+}
