6c4a81d699c7032e037c502ed1983227516984a9
hntduong12345 <115139619+hntduong12345@users.noreply.github.com>
2026-01-31 11:51:49 +0700

    feat: add Find Smallest Letter Greater Than Target code

---
+++ Find Smallest Letter Greater Than Target.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public char NextGreatestLetter(char[] letters, char target)
+    {
+        char res = letters[0];
+        int minDif = letters[0] - target > 0 ? letters[0] - target : 26;
+        int dif = 0;
+        for (int i = 0; i < letters.Length; i++)
+        {
+            dif = letters[i] - target;
+            if (dif > 0 && dif < minDif)
+            {
+                minDif = dif;
+                res = letters[i];
+            }
+        }
+
+        return res;
+    }
+}
