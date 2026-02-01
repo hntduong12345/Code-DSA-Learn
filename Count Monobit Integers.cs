92702cd50ef2c2a4feb1293d5228d924c1e92d77
hntduong12345 <115139619+hntduong12345@users.noreply.github.com>
2026-02-01 14:14:42 +0700

    feat: add Count Monobit Integers code

---
+++ Count Monobit Integers.cs
@@ -0,0 +1,18 @@
+﻿public class Solution
+{
+    public int CountMonobit(int n)
+    {
+        int res = 1;
+        int mul = 2;
+
+        int num = 1;
+        while (num <= n)
+        {
+            res++;
+            num += mul;
+            mul *= 2;
+        }
+
+        return res;
+    }
+}
