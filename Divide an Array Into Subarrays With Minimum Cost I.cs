01fd3c1c6e39e304dc09f9d2353d548b19f5ef26
hntduong12345 <115139619+hntduong12345@users.noreply.github.com>
2026-02-01 13:14:24 +0700

    feat: add Divide an Array Into Subarrays With Minimum Cost I code

---
+++ Divide an Array Into Subarrays With Minimum Cost I.cs
@@ -0,0 +1,24 @@
+﻿public class Solution
+{
+    public int MinimumCost(int[] nums)
+    {
+        int sum = nums[0];
+        int min = int.MaxValue;
+        int secondMin = int.MaxValue;
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] < min)
+            {
+                secondMin = min;
+                min = nums[i];
+            }
+            else if (nums[i] < secondMin)
+            {
+                secondMin = nums[i];
+            }
+        }
+
+        return sum + min + secondMin;
+    }
+}
