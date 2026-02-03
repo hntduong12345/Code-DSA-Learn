100fb0959547f885f651fda8e4fc46cb7fe254c6
hntduong12345 <115139619+hntduong12345@users.noreply.github.com>
2026-02-03 21:20:42 +0700

    feat: add Trionic Array I code

---
+++ Trionic Array I.cs
@@ -0,0 +1,34 @@
+﻿public class Solution
+{
+    public bool IsTrionic(int[] nums)
+    {
+        int n = nums.Length;
+        int i = 0;
+
+        //Increase side 1
+        while (i + 1 < n && nums[i] < nums[i + 1])
+        {
+            i++;
+        }
+
+        if (i == 0 || i == n - 1)
+            return false;
+
+        //Decrease side
+        while (i + 1 < n && nums[i] > nums[i + 1])
+        {
+            i++;
+        }
+
+        if (i == n - 1)
+            return false;
+
+        //Increase side 2
+        while (i + 1 < n && nums[i] < nums[i + 1])
+        {
+            i++;
+        }
+
+        return i == n - 1;
+    }
+}
