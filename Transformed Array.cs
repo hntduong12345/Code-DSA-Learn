4152d8bea7a42bfab2751cae62e45d6c35d356bb
hntduong12345 <115139619+hntduong12345@users.noreply.github.com>
2026-02-13 19:47:26 +0700

    feat: add Transformed Array code

---
+++ Transformed Array.cs
@@ -0,0 +1,28 @@
+﻿public class Solution
+{
+    public int[] ConstructTransformedArray(int[] nums)
+    {
+        int n = nums.Length;
+        int[] res = new int[n];
+        int targetIndex = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] > 0)
+            {
+                targetIndex = (i + nums[i]) % n;
+                res[i] = nums[targetIndex];
+            }
+            else if (nums[i] < 0)
+            {
+                targetIndex = (i + nums[i] + n) % n;
+                while (targetIndex < 0)
+                {
+                    targetIndex = (targetIndex + n) % n;
+                }
+                res[i] = nums[targetIndex];
+            }
+        }
+
+        return res;
+    }
+}
