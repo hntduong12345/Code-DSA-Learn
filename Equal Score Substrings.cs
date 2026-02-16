6677236312eaa62842a941400f0fa90a8c926a71
hntduong12345 <115139619+hntduong12345@users.noreply.github.com>
2026-02-16 13:06:58 +0700

    feat: add Equal Score Substrings code

---
+++ Equal Score Substrings.cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+    public bool ScoreBalance(string s)
+    {
+        int[] preSum = new int[s.Length];
+        int n = s.Length;
+        preSum[0] = s[0] - 'a' + 1;
+        for (int i = 1; i < n; i++)
+        {
+            preSum[i] += preSum[i - 1] + (s[i] - 'a' + 1);
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            if (preSum[i] * 2 == preSum[n - 1])
+                return true;
+        }
+
+        return false;
+    }
+}
