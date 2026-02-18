370b06426c5b73a46f233bb6bd834a1965e6b97f
hntduong12345 <115139619+hntduong12345@users.noreply.github.com>
2026-02-18 09:59:51 +0700

    feat: add Binary Number with Alternating Bits code

---
+++ Binary Number with Alternating Bits.cs
@@ -0,0 +1,19 @@
+﻿public class Solution
+{
+    public bool HasAlternatingBits(int n)
+    {
+        int bit = n % 2 == 0 ? 0 : 1;
+        n /= 2;
+
+        while (n > 0)
+        {
+            if (n % 2 == bit)
+                return false;
+            else
+                bit = n % 2;
+            n /= 2;
+        }
+
+        return true;
+    }
+}
