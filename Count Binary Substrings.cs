8a72b4f07dc6db7534459244dc8df9460e76f4b0
hntduong12345 <115139619+hntduong12345@users.noreply.github.com>
2026-02-19 09:04:21 +0700

    feat: add Count Binary Substrings code

---
+++ Count Binary Substrings.cs
@@ -0,0 +1,22 @@
+﻿public class Solution
+{
+    public int CountBinarySubstrings(string s)
+    {
+        int res = 0;
+        int prev = 0, cur = 1;
+        for (int i = 1; i < s.Length; i++)
+        {
+            if (s[i] != s[i - 1])
+            {
+                res += Math.Min(prev, cur);
+                prev = cur;
+                cur = 1;
+            }
+            else
+            {
+                cur++;
+            }
+        }
+        return res + Math.Min(prev, cur);
+    }
+}
