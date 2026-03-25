f6eea73a97da0634aad12ba3a7f4a1d9888c212c
hntduong12345 <115139619+hntduong12345@users.noreply.github.com>
2026-03-25 20:34:44 +0700

    feat: add missing code

--- Equal Sum Grid Partition I.cs
+++ Equal Sum Grid Partition I.cs
@@ -1,8 +1,44 @@
-﻿using System;
-
-public class Class1
+﻿public class Solution
 {
-	public Class1()
-	{
-	}
+    public bool CanPartitionGrid(int[][] grid)
+    {
+        int m = grid.Length;
+        int n = grid[0].Length;
+
+        long[] horizontal = new long[m];
+        long[] vertical = new long[n];
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                horizontal[i] += grid[i][j];
+                vertical[j] += grid[i][j];
+            }
+        }
+
+        for (int i = 1; i < m; i++)
+        {
+            horizontal[i] += horizontal[i - 1];
+        }
+
+        for (int i = 1; i < n; i++)
+        {
+            vertical[i] += vertical[i - 1];
+        }
+
+        for (int i = 0; i < m - 1; i++)
+        {
+            if (horizontal[i] * 2 == horizontal[m - 1])
+                return true;
+        }
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            if (vertical[i] * 2 == vertical[n - 1])
+                return true;
+        }
+
+        return false;
+    }
 }
